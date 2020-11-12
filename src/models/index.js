b5ea2ad6ab1ca418918c0b8b39c21ed718e2bdf0
TedBurns <ted@TedBurnss-MacBook-Pro.local>
2020-11-12 17:27:42 -0500

    adding orm

--- src/models/index.js
+++ src/models/index.js
@@ -1,28 +1,27 @@
-let users = {
-  1: {
-    id: "1",
-    username: "Robin Wieruch",
-  },
-  2: {
-    id: "2",
-    username: "Dave Davids",
-  },
+import Sequelize from "sequelize";
+import user from "./user";
+import message from "./message";
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: "postgres",
+  }
+);
+
+const models = {
+  User: user(sequelize, Sequelize.DataTypes),
+  Message: message(sequelize, Sequelize.DataTypes),
 };
 
-let messages = {
-  1: {
-    id: "1",
-    text: "Hello World",
-    userId: "1",
-  },
-  2: {
-    id: "2",
-    text: "By World",
-    userId: "2",
-  },
-};
+Object.keys(models).forEach((key) => {
+  if ("associate" in models[key]) {
+    models[key].associate(models);
+  }
+});
 
-export default {
-  users,
-  messages,
-};
+export { sequelize };
+
+export default models;
