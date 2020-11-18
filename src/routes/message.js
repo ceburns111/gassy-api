f9defe2cb0e49f23afb502b213b149d41e122927
Christopher (Ted) Burns <ceburns111@gmail.com>
2020-11-18 15:06:47 -0500

    finishing tut/template

--- src/routes/message.js
+++ src/routes/message.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { BadRequestError } from '../utils/errors';
 
 const router = Router();
 
@@ -14,11 +15,11 @@ router.get("/:messageId", async (req, res) => {
   return res.send(message);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   const message = await req.context.models.Message.create({
     text: req.body.text,
     userId: req.context.me.id,
-  });
+  }).catch((error) => next(new BadRequestError(error)));
 
   return res.send(message);
 });
