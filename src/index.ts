24d66f5a0e8eaffb59bbea5d75264862e03413a0
Christopher (Ted) Burns <ceburns111@gmail.com>
2020-11-19 17:42:08 -0500

    converted src/index to ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import models, { sequelize } from "./models";
 import routes from "./routes";
+import HttpException from "./utils/httpexception";
 
 const app = express();
 
@@ -13,9 +14,23 @@ app.use("/users", routes.user);
 app.use("/messages", routes.message);
 
 app.get("*", function (req, res, next) {
-  const error = new Error(`${req.ip} tried to access ${req.originalUrl}`);
-
-  error.statusCode = 301;
-
+  const error = new HttpException(
+    301,
+    `${req.ip} tried to access ${req.originalUrl}`
+  );
   next(error);
 });
+
+app.use((error: HttpException, req, res, next) => {
+  if (!error.statusCode) error.statusCode = 500;
+
+  if (error.statusCode === 301) {
+    return res.status(301).redirect("/not-found");
+  }
+
+  return res.status(error.statusCode).json({ error: error.message });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`App is listening on port ${process.env.PORT}`)
+);
